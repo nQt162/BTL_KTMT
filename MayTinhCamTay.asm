1df9f0b001dfd4a2a26add529437643c88ed781a
Nguyễn Quốc Triệu <phapdanhthichdaik@gmail.com>
2025-05-24 18:32:05 +0700

    Update MayTinhCamTay.asm

--- MayTinhCamTay.asm
+++ MayTinhCamTay.asm
@@ -1,210 +1,425 @@
-.Model Small
-.Stack 100h
-.Data
-    msg db 0dh,0ah,"1-Add", 0dh,0ah,"2-Multiply", 0dh,0ah,"3-Subtract", 0dh,0ah,"4-Divide", 0dh,0ah,"5-Exit", 0Dh,0Ah, '$'
-    msg1 db 0dh, 0ah, "Select type: $"
-    msg2 db 0dh,0ah,"Enter First No : $"
-    msg3 db 0dh,0ah,"Enter Second No : $"
-    msg4 db 0dh,0ah,"Choice Error $" 
-    msg5 db 0dh,0ah,"Result : $" 
-    msg6 db 0dh,0ah ,"Thank you for using the calculator!$ "
-    crlf db 13, 10, '$'
-             
-    base_decmical dw 10 
-    x dw 0
-             
-    a dw 0
-    b dw 0 
-    res dw 0 
-    choice db 0
-.Code    
-Main PROC
-    mov ax, @Data
-    mov ds, ax 
+.model small 
+.stack 100h 
+.data
+   
+x_float_count db 0
+x        dw 0    
+y        dw 0
+buffer   db 6 dup(0),'$'
+lenth    dw 0
+operand1 db 0
+operand2 db 0
+key      db 0  
+xsighn   db 0
+;---------------------------------------------------
+ 
+;print lines
+;---------------------------------------------------                           
+    l0   db 201,21 dup(205),187,'$'
+    l8   db 186,21 dup(' '),186,'$';second line
     
-    lea dx, msg
-    mov ah, 9
-    int 21h 
+    l1   db 186,'   ',201,13 dup(205),187,'   ',186,'$'
+    l2   db 186,'   ',186,13 dup(' '),186,'   ',186,'$'
+    l3   db 186,'   ',200,13 dup(205),188,'   ',186,'$'
+    
+    l4   db 186,' ',218,196,191,' ',218,196,191,' ',218,196,191,' ',218,196,191,' ',218,196,191,' ',186,'$'
+    l4_2 db 186,' ',218,9 dup(196),191,' ',218,196,191,' ',179,' ',179,' ',186,'$'
+    l5   db 186,' ',179,' ',179,' ',179,' ',179,' ',179,' ',179,' ',179,' ',179,' ',179,' ',179,' ',186,'$'
+    l5_2 db 186,' ',192,196,217,' ',192,196,217,' ',192,196,217,' ',192,196,217,' ',179,' ',179,' ',186,'$'
+    l5_3 db 186,' ',179,9 dup(' '),179,' ',179,' ',179,' ',179,' ',179,' ',186,'$'
+    l6   db 186,' ',192,196,217,' ',192,196,217,' ',192,196,217,' ',192,196,217,' ',192,196,217,' ',186,'$'
+    l6_2 db 186,' ',192,9 dup(196),217,' ',192,196,217,' ',192,196,217,' ',186,'$'
+    l7   db 200,21 dup(205),188,'$'
+;---------------------------------------------------                           
+ends
 
-    mainLoop: 
-        lea dx, msg1
-        mov ah, 9
-        int 21h
-        
-        mov ah, 01h
-        int 21h
-        mov [choice], al
+;---------------------------------------------------
+gotoxy macro x,y        ;move cursor
+       pusha
+       mov dl,x
+       mov dh,y
+       mov bx,0
+       mov ah,02h
+       int 10h 
+       popa
+endm
+;--------------------------------------------------- 
+putstr macro buffer     ;print string
+       pusha
+       mov ah,09h
+       mov dx,offset buffer
+       int 21h 
+       popa
+endm
+;---------------------------------------------------
+putch  macro char,color ;print character
+       pusha
+       mov ah,09h
+       mov al,char
+       mov bh,0
+       mov bl,color
+       mov cx,1
+       int 10h 
+       popa
+endm
+;---------------------------------------------------
+clear  macro buf
+       lea si,buf
+       mov [si],' '
+       mov [si+1],' '
+       mov [si+2],' '
+       mov [si+3],' '
+       mov [si+4],' '
+       mov [si+5],' '
+       gotoxy 15,3
+       putstr buf
+endm
+;---------------------------------------------------
+number_in  macro  n,operand,lenth
+    local l1,l1_2,l1_2_1,l1_3
+    local l2,l3,l4
+    local next_step, store_operand
+
+    pusha
+    
+      
+    mov lenth, 0  
+    lea si, buffer         
+    
+l1:
+    mov ah, 08h            ;nhap ki tu vao ban phim 
+    int 21h
+    mov key, al
+    
+l1_2: 
+    cmp lenth, 0
+    jne next_step
+    clear buffer       
+    putstr buffer          ;in ra chuoi rong , ghi de len man hinh
+    gotoxy 17,3
+
+next_step:
+    cmp al, '0'            ; kiem tra xem ki tu co phai la so
+    jb l2
+    cmp al, '9'
+    ja l2
+
+l1_3:
+    mov al, key
+    mov [si], al
+    pusha
+    mov dl, al
+    mov ah, 02h            ; in ra so da nhap vao 
+    int 21h
+    popa
+    inc si
+    inc lenth
+    jmp l1
+
+; ==============================
+l2: 
+    cmp al, '+'
+    je store_operand
+    cmp al, '-'
+    je store_operand
+    cmp al, '*'
+    je store_operand
+    cmp al, '/'
+    je store_operand
+    cmp al, '='
+    je store_operand
+    jmp l1  
+
+; ==============================
+store_operand:
+    cmp lenth, 0
+    je l4  
+    gotoxy 26,3
+    mov operand, al  
+
+ 
+    mov dl, operand           ;in ra toan tu ( + - * / ) da nhap 
+    mov ah, 02h
+    int 21h
 
    
-        cmp choice, '5'
-        je exit
-        cmp choice, '1'
-        je do_add
-        cmp choice, '2'
-        je do_mul
-        cmp choice, '3'
-        je do_sub
-        cmp choice, '4'
-        je do_div
-    
-        lea dx, msg4
-        mov ah, 9
-        int 21h
-        lea dx, crlf
-        mov ah, 9
-        int 21h
-        
-        jmp mainLoop    
+    lea si, buffer
+    mov cx, lenth
+    mov dx, 0
+    mov bx, 10
 
-    do_add:  
-        call ReadNum
-        call Addition
-        jmp show_result
-    do_mul:           
-        call ReadNum
-        call Multiply
-        jmp show_result
-    do_sub:           
-        call ReadNum
-        call Substract
-        jmp show_result
-    do_div:          
-        call ReadNum
-        call Divide
-        jmp show_result
-
-    show_result:
-        lea dx, msg5
-        mov ah, 9
-        int 21h
-        
-        call OutputInteger  
-         
-        lea dx, crlf
-        mov ah, 9
-        int 21h      
-        
-        jmp mainLoop
-
-    exit:
-        lea dx, msg6
-        mov ah, 9
-        int 21h
-
-        mov ah, 4Ch
-        int 21h
-Main ENDP
-      
-
-InputInteger PROC
-    mov x, 0
-    
-    loopDigit:
-        mov ah, 1
-        int 21h
-        
-        cmp al, 13
-        je endInput
-        cmp al, ' '
-        je endInput
-        
-        mov ah, 0           ; reset 
-        sub al, '0'         ; convert from char to int
-        push ax
-        
-        mov ax, x
-        mul base_decmical
-        mov x, ax
-        pop ax
-        add x, ax
-        
-        jmp loopDigit  
-    
-    endInput:
-    ret        
-        
-InputInteger ENDP
-
-ReadNum PROC
-    lea dx, msg2
-    mov ah, 9
-    int 21h
-    call InputInteger
-    mov ax, [x]
-    mov [a], ax 
-
-    lea dx, msg3
-    mov ah, 9
-    int 21h
-    call InputInteger
-    mov ax, [x]
-    mov [b], ax
-    
-    ret     
-    
-ReadNum ENDP
-
-OutputInteger PROC
-    mov ax, res
-    mov cx, 0
-    
-    GetDigit:
-        mov dx, 0           ; reset
-        div base_decmical
-        push dx
-        
-        inc cx  
-        cmp ax, 0
-        je showInteger  
-        
-                jmp GetDigit
-    showInteger:
-        mov ah, 2
-        
-        pop dx
-        add dl, '0'
-        
-        int 21h
-        
-        dec cx
-        cmp cx, 0
-        jne showInteger
-ret
-
-OutputInteger ENDP
- 
-Addition PROC
-    mov ax, [a]
-    add ax, [b] 
-    mov [res], ax   
-                 
-    ret
-Addition ENDP  
-
-Substract PROC
-    mov ax, [a]
-    sub ax, [b] 
-    mov [res], ax   
-                 
-    ret 
-Substract ENDP  
-    
-Multiply PROC
-    mov ax, [a]
-    mov bx, [b] 
+l3:                          ; in ra ket qua 
+    mov ax, dx
     mul bx
-    mov [res], ax   
-                 
-    ret 
-Multiply ENDP
+    mov dx, ax
+    mov ah, 0
+    mov al, [si]
+    sub al, 48
+    add dx, ax
+    inc si
+    loop l3
 
-Divide PROC
-    mov ax, [a]
-    mov bx, [b]
-    div bx 
-    mov [res], ax   
-                 
-    ret 
-Divide ENDP  
+    mov n, dx
 
-END MAIN
+l4:
+    mov n, dx
+    gotoxy 24,3
+    popa
+endm
+
+ 
+     
+;---------------------------------------------------
+putrez macro buffer,x
+       local next_digit,pz1,pz2
+       local nex1,nex2
+     
+       pusha
+       mov ax,x             
+       mov cl,x_float_count
+       clear buffer
+       lea si,buffer               
+       mov bx,10             
+       mov [si+5],'0'
+       mov [si+4],'.'
+       mov [si+3],'0'
+       
+    ;........................                         
+       next_digit: 
+        cmp cl,0
+        jne nex1
+        cmp x_float_count,0
+        jne nex2
+        
+        mov [si+5],'0'
+        dec si
+    nex2:    
+        mov dl,'.'
+        mov [si+5],dl
+        dec si
+        dec cl
+        dec x_float_count
+        jmp next_digit
+         
+    nex1:
+        mov dx,0            ; buffer 
+        div bx             
+        add dl,48          
+        mov [si+5],dl
+        dec si
+        dec cl
+        cmp ax,0
+        jne next_digit
+         
+     
+   ;.........................          
+       gotoxy 17,3          ;print buffer
+       putstr buffer 
+        
+      
+       
+   pz1:
+       cmp xsighn,1
+       jne pz2
+       gotoxy 15,3
+       putch '-' , 15
+   pz2:
+       popa
+endm
+;---------------------------------------------------
+reset macro
+       mov x,0
+       mov y,0 
+       mov xsighn,0       
+       clear buffer
+       mov key,0
+       mov operand1,0
+       mov operand2,0
+endm
+
+
+.code
+
+start:
+; set segment registers:
+    mov ax, @data
+    mov ds, ax
+    
+ 
+
+
+call print_screen    
+ 
+begin:
+      reset
+      calc1:
+          
+          number_in x,operand1,lenth
+          mov al,operand1
+          cmp al,'='  
+          je calc1          
+          
+      calc2:
+          number_in y,operand2,lenth
+          mov al,operand2
+          cmp al,'='
+          jne calc1          ;if not '=', restart (invalid input)
+          call calculate
+              
+          putrez buffer,x    ;display result
+          mov x_float_count, 0
+          mov xsighn,0
+          jmp calc1          ;start again for new calculation
+ 
+
+;--------------------------------------------------
+ 
+print_screen proc :
+        gotoxy 10,0;...........
+        putstr l0
+        gotoxy 10,1
+        putstr l8
+        gotoxy 10,2;........  ;
+        putstr l1          ;  ;
+        gotoxy 10,3        ;  ;out cadr
+        putstr l2          ;  ;
+        gotoxy 10,4        ;  ;
+        putstr l3          ;  ;
+        gotoxy 10,5;...... ;  ;
+        putstr l4        ; ;result board + exit key
+        gotoxy 10,6      ; ;  ;
+        putstr l5        ; ;  ;
+        gotoxy 10,7      ; ;  ;
+        putstr l6        ; ;  ;
+        gotoxy 10,8      ; ;  ;
+        putstr l4        ; ;  ;
+        gotoxy 10,9      ;key board
+        putstr l5        ; ;  ;
+        gotoxy 10,10     ; ;  ;
+        putstr l6        ; ;  ;
+        gotoxy 10,11     ; ;  ;
+        putstr l4        ; ;  ;
+        gotoxy 10,12     ; ;  ;
+        putstr l5        ; ;  ;
+        gotoxy 10,13     ; ;  ;
+        putstr l5_2      ; ;  ;
+        gotoxy 10,14     ; ;  ;
+        putstr l4_2      ; ;  ;
+        gotoxy 10,15;..... ;  ;
+        putstr l5_3      ;  ;
+        gotoxy 10,16;.......  ;  
+        putstr l6_2      ;
+        gotoxy 10,17    
+        putstr l7 
+        
+         
+        ;keyboard labels
+        
+        
+        gotoxy 13,6
+        putch '7',14
+        gotoxy 17,6
+        putch '8',14
+        gotoxy 21,6
+        putch '9',14
+        gotoxy 25,6
+        putch '/',11
+        gotoxy 29,6
+        putch 'C',5
+        gotoxy 13,9
+        putch '4',14
+        gotoxy 17,9
+        putch '5',14
+        gotoxy 21,9
+        putch '6',14
+        gotoxy 25,9
+        putch '*',11
+        gotoxy 29,9
+        putch 241,11
+        gotoxy 13,12
+        putch '1',14
+        gotoxy 17,12
+        putch '2',14
+        gotoxy 21,12
+        putch '3',14
+        gotoxy 25,12
+        putch '-',11
+        gotoxy 29,13
+        putch '=',11
+        gotoxy 17,15
+        putch '0',14
+      
+        gotoxy 25,15
+        putch '+',11        
+          
+       
+print_screen endp
+;--------------------------------------------------
+ 
+calculate proc 
+    
+    cmp operand1,'+'
+    je plus
+    cmp operand1,'-'
+    je minus
+    cmp operand1,'*'
+    je mult
+    cmp operand1,'/'
+    je devide
+    jmp begin   ;no match found!
+
+            plus:              
+                        mov ax,x
+                        add ax,y                        
+                            mov x,ax
+                            ret                   
+                                                                              
+            minus:                                
+                mov ax,x
+                cmp ax,y
+                jae mi3
+                jb mi4
+                
+                mi3:            ; neu x >= y 
+                    mov ax,x
+                    sub ax,y
+                    mov x,ax
+                    mov xsighn,0
+                    ret
+                mi4:            ; neu x  < y , cho xsighn = 1 de lam dau - ;
+                    mov ax,y
+                    sub ax,x
+                    mov x,ax
+                    mov xsighn,1
+                    ret
+                    
+            mult:
+                    mov ax, x
+                    mov bx, y
+                    mul bx       
+                    mov x, ax
+                    ret
+       
+                   
+                            
+            devide:
+                    xor dx, dx
+                    mov ax, x
+                    mov bx, 10
+                    mul bx           
+                    mov bx, y
+                    xor dx, dx
+                    div bx           
+                    mov x, ax        ;
+                    mov x_float_count, 1  ; 
+                    ret                    
+calculate endp
+;---------------------------------------------------                           
+ 
+exit:
+ 
+    mov ax, 4c00h ; exit 
+    int 21h    
+ends
+ 
+end start
